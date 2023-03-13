9258abd8b38bc261c86a2f92c91d8f7a5be2a90b
Amir Rachum <amir@rachum.com>
2023-03-13 08:07:16 +0200

    Starting to work on a telegram bot.

---
+++ src/telegram_bot.py
@@ -0,0 +1,7 @@
+import logging
+
+from telegram import Update, ForceReply, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+
+def main():
+    pass
