03d1a717877516a0c07d40bf05dac093c80acbca
Amir Rachum <amir@rachum.com>
2023-03-20 17:53:04 +0200

    Telegram bot: record events properly.

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -6,24 +6,32 @@ import os
 import csv
 import logging
 from enum import Enum
-from typing import List, NamedTuple
+from typing import List, NamedTuple, Optional
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.INFO,
 )
+logger = logging.getLogger(__name__)
 
 
-class EventType(Enum):
+class ActivityType(Enum):
     UNKNOWN = 0
     JOURNAL = 1
 
 
+class EventType(Enum):
+    UNKNOWN = 0
+    DONE = 1
+    SKIPPED = 2
+
+
 class Event(NamedTuple):
     date: datetime.date
+    activity_type: ActivityType
     event_type: EventType
 
 
@@ -33,30 +41,39 @@ class EventCsvStorage:
         self._events: List[Event] = self._load_from_storage()
 
     def _load_from_storage(self) -> List[Event]:
+        if not self._filepath.exists():
+            return []
+
         events = []
         with open(self._filepath, 'r') as f:
             reader = csv.reader(f)
             for row in reader:
                 if row:
-                    events.append(Event(datetime.date.fromisoformat(row[0]), EventType[row[1]]))
+                    events.append(Event(datetime.date.fromisoformat(row[0]), ActivityType[row[1]], EventType[row[2]]))
         return events
 
     def add(self, event: Event):
         self._events.append(event)
         self._write_to_storage()
 
-    def exists(self, event):
-        return event in self._events
+    def find_event(self, activity_type: ActivityType, date: datetime.date) -> Optional[EventType]:
+        for event in self._events:
+            if event.activity_type == activity_type and event.date == date:
+                return event.event_type
 
     def _write_to_storage(self):
         with open(self._filepath, 'w') as f:
             writer = csv.writer(f)
-            writer.writerows((event.date, event.event_type.name) for event in self._events)
+            writer.writerows((event.date, event.activity_type.name, event.event_type.name) for event in self._events)
 
 
 async def ask_journal(context: ContextTypes.DEFAULT_TYPE):
     event_storage: EventCsvStorage = context.application.user_data[context.job.chat_id][EventCsvStorage.__name__]
-    if event_storage.exists(Event(datetime.date.today(), EventType.JOURNAL)):
+    activity_type: ActivityType = ActivityType.JOURNAL
+
+    if (event_type := event_storage.find_event(ActivityType.JOURNAL, datetime.date.today())) is not None:
+        logger.info(f'Skipping asking for {activity_type} because its '
+                     f'status is {event_type}')
         return
 
     keyboard = [
@@ -86,13 +103,15 @@ async def button(update: Update,
 
     if query.data == "1":
         text = "Awesome!"
-        context.user_data[EventCsvStorage.__name__].add(Event(datetime.date.today(), EventType.JOURNAL))
+        context.user_data[EventCsvStorage.__name__].add(Event(datetime.date.today(), ActivityType.JOURNAL, EventType.DONE))
     elif query.data == "2":
         text = "Sure, I'll ask again later."
     elif query.data == "3":
+        context.user_data[EventCsvStorage.__name__].add(Event(datetime.date.today(), ActivityType.JOURNAL, EventType.SKIPPED))
         text = "A'ight, I won't bother you again today."
 
     await query.edit_message_text(text=text)
+
 
 def remove_job_if_exists(name: str,
                          context: ContextTypes.DEFAULT_TYPE) -> bool:
@@ -109,7 +128,7 @@ async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_message.chat_id
 
     remove_job_if_exists(str(chat_id), context)
-    context.job_queue.run_once(ask_journal, 3, chat_id=chat_id,
+    context.job_queue.run_repeating(ask_journal, datetime.timedelta(seconds=10), chat_id=chat_id,
                                name=str(chat_id))
 
     context.user_data.setdefault(EventCsvStorage.__name__, EventCsvStorage())
