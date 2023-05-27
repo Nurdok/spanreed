90dae66f2b9db84649188de09e1b63ba39c66932
Amir Rachum <amir@rachum.com>
2023-05-27 15:16:38 +0300

    host/port

--- src/spanreed/plugins/web_ui.py
+++ src/spanreed/plugins/web_ui.py
@@ -75,7 +75,7 @@ class WebUiPlugin(Plugin[None]):
                     await websocket.send(message["data"].decode("utf-8"))
 
         # Start the Quart app.
-        await app.run_task(debug=True)
+        await app.run_task(debug=True, host="0.0.0.0", port=80)
 
 
 class RedisPubSubHandler(logging.Handler):
