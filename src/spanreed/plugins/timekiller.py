e31b3060b1ec8d343747ccc7eb08588ba0b47883
Amir Rachum <amir@rachum.com>
2023-12-18 08:41:24 +0200

    more timekiller

--- src/spanreed/plugins/timekiller.py
+++ src/spanreed/plugins/timekiller.py
@@ -1,5 +1,5 @@
 import asyncio
-
+import random
 import datetime
 
 from spanreed.apis.telegram_bot import TelegramBotApi, PluginCommand
@@ -27,3 +27,35 @@ class TimekillerPlugin(Plugin):
 
         async with bot.user_interaction():
             pass
+
+    async def _journal_prompt(self, user: User, bot: TelegramBotApi) -> None:
+        prompts: list[str] = [
+            "What are you doing right now?",
+            "What are you grateful for today?",
+            "What are you looking forward to today?",
+            "What are you struggling with today?",
+            "What are you proud of today?",
+            "What are you excited about today?",
+            "What are you worried about today?",
+            "Did you make progress on a project today? If so, what?",
+            "What is one thing you learned today?",
+            "What did you do today to take care of yourself?",
+            "What did you do today to take care of someone else?",
+            "What did you do today to take care of your home?",
+            "What friend did you talk to today?",
+            "What family member did you talk to today?",
+            "What did you do today for your yearly theme?",
+            "Take a picture of you or something you did today.",
+            "What did you do for physical health today?",
+            "What did you do for mental health today?",
+            "What did you do today for fun?",
+            "What book are you currently reading?",
+            "What are you currently watching?",
+            "What are you currently listening to?",
+            "What is your favorite song right now?",
+            "Do you have plans to meet up with friends anytime soon?",
+            "What project are you currently working on?",
+        ]
+
+        prompt = random.choice(prompts)
+        prompt_answer: str = await bot.request_user_input(prompt)
